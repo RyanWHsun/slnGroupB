233de163ccbbfbbb38fc58acf63a0b501136acec
eren <ahm12345@gmail.com>
2024-11-15 21:35:36 +0800

    新增 CAttraction.cs 和 CAttractionImage.cs 2個 class

--- prjGroupB/Models/CAttraction.cs
+++ prjGroupB/Models/CAttraction.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace Attractions.Models {
     public class CAttraction {
-        public int fAttractionId {  get; set; }
+        public int fAttractionId { get; set; }
         public string fAttractionName { get; set; }
         public string fDescription { get; set; }
-        public string fAddress {  get; set; }
+        public string fAddress { get; set; }
         public string fPhoneNumber { get; set; }
         public TimeSpan fOpeningTime { get; set; }
         public TimeSpan fClosingTime { get; set; }
-        public string fWebsiteURL {  get; set; }
+        public string fWebsiteURL { get; set; }
         public string fLongitude { get; set; }
         public string fLatitude { get; set; }
         public string fRegion { get; set; }
@@ -21,7 +21,7 @@ namespace Attractions.Models {
         public string fCategoryName { get; set; }
         public DateTime fCreatedDate { get; set; }
         public DateTime fUpdatedDate { get; set; }
-        public string fStatus {  get; set; }
+        public string fStatus { get; set; }
         public string fTransformInformation { get; set; }
     }
 }
